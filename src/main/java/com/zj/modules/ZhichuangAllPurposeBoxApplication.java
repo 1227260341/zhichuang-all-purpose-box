3e2035f47fc0fbb71ca34c730b911c97f088967e
aaa <1227260341@qq.com>
2019-08-23 16:35:30 +0800

    。

---
+++ src/main/java/com/zj/modules/ZhichuangAllPurposeBoxApplication.java
@@ -0,0 +1,13 @@
+package com.zj.modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhichuangAllPurposeBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhichuangAllPurposeBoxApplication.class, args);
+	}
+
+}
